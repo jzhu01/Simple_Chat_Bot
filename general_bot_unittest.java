e8193fa99a1f772f46e02daccb1d8cfca694ebb3
Jennifer Zhu <jzhu@smith.edu>
2016-01-11 17:04:36 -0500

    Update general_bot_unittest.java

--- general_bot_unittest.java
+++ general_bot_unittest.java
@@ -1,1 +1,21 @@
+import junit.framework.*;
+
+/**
+ * Class for unit testing generalBot
+ *
+ * @author: Jennifer Zhu
+ * @version: 1/7/15
+ */
+
+public class generalBot_unittest extends TestCase {
+
+  @Test
+  public void testPrintFirstLine() {
+    // set a string of what we expect here equa to message
+    // set another equal to what calling generalBot's methoud should print out 
+    assertEquals(message, messageUtil.printMessage());
+  }
+
+
+}
 
